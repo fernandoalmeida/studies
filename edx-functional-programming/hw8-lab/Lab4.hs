b81d581a375912ee97db3a87f26c8472d4cfad66
Fernando Almeida <fernando@fernandoalmeida.net>
2016-03-03 00:57:35 -0300

    [fp-haskell] Homework 8 (lab) - Recursive Functions

--- edx-functional-programming/hw8-lab/Lab4.hs
+++ edx-functional-programming/hw8-lab/Lab4.hs
@@ -1,24 +1,27 @@
 module Lab4 where
 
-------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- RECURSIVE FUNCTIONS
-------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 import Data.Char
+import Foreign.Marshal.Utils
 
 -- ===================================
 -- Ex. 0
 -- ===================================
 
-triangle :: Integer -> Integer
-triangle n = undefined
+-- triangle :: Integer -> Integer
+-- triangle 0 = 0
+-- triangle (n + 1) = (n + 1) + triangle n
 
 -- ===================================
 -- Ex. 1
 -- ===================================
 
 count :: Eq a => a -> [a] -> Int
-count x xs = undefined
+count n [] = 0
+count n (x : xs) = fromBool (x == n) + count n xs
 
 xs = [1,2,35,2,3,4,8,2,9,0,5,2,8,4,9,1,9,7,3,9,2,0,5,2,7,6,92,8,3,6,1,9,2,4,8,7,1,2,8,0,4,5,2,3,6,2,3,9,8,4,7,1,4,0,1,8,4,1,2,4,56,7,2,98,3,5,28,4,0,12,4,6,8,1,9,4,8,62,3,71,0,3,8,10,2,4,7,12,9,0,3,47,1,0,23,4,8,1,20,5,7,29,3,5,68,23,5,6,3,4,98,1,0,2,3,8,1]
 ys = map (\x -> ((x + 1) * 3) ^ 3 - 7) xs
@@ -38,11 +41,24 @@ poem = [ "Three Types for the Lisp-kings under the parentheses,"
 -- ===================================
 
 euclid :: (Int,  Int) -> Int
-euclid (x, y) = undefined
+euclid (x, 0) = undefined
+euclid (0, y) = undefined
+euclid (x, y) = if x == y then x else euclid ((min x y), (max x y) - (min x y))
 
 -- ===================================
 -- Ex. 3
 -- ===================================
 
 funkyMap :: (a -> b) -> (a -> b) -> [a] -> [b]
-funkyMap f g xs = undefined
+funkyMap _ _ [] = []
+funkyMap f _ (x : []) = [f x]
+funkyMap f g (x : x' : xs) = f x : g x' : funkyMap f g xs
+
+dup :: a -> (a, a)
+dup a = (a, a)
+
+-- k :: (Num a) => a -> a
+h g f = (f . g) $ f
+fix = h fix
+f = \f n -> if (n == 0) then 1 else n * f (n - 1)
+k = fix $ f
