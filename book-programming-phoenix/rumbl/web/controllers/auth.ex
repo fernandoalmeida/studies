0f48f50b59f2f1f5f3929cb7590ffc24a24c7811
Fernando Almeida <fernando@fernandoalmeida.net>
2016-06-11 20:40:31 -0300

    [book-phoenix] Set `current_user` via `Auth` plug

--- book-programming-phoenix/rumbl/web/controllers/auth.ex
+++ book-programming-phoenix/rumbl/web/controllers/auth.ex
@@ -1,5 +1,17 @@
 defmodule Rumbl.Auth do
+  import Plug.Conn
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Rumbl.User, user_id)
+
+    assign(conn, :current_user, user)
+  end
 
   def login(conn, user) do
     conn
