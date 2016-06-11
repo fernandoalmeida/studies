8690841ee2a34220df1d5b38785f748960c502c4
Fernando Almeida <fernando@fernandoalmeida.net>
2016-06-11 18:40:48 -0300

    [book-phoenix] Add user creation page

--- book-programming-phoenix/rumbl/web/router.ex
+++ book-programming-phoenix/rumbl/web/router.ex
@@ -16,9 +16,8 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/users/:id", UserController, :show
-    get "/users", UserController, :index
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
