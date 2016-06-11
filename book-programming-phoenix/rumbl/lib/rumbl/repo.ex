c21b837767d32033502bad10ea8860202a9a1673
Fernando Almeida <fernando@fernandoalmeida.net>
2016-06-11 18:13:50 -0300

    [book-phoenix] Return `Ecto` on `Repo` configuration

--- book-programming-phoenix/rumbl/lib/rumbl/repo.ex
+++ book-programming-phoenix/rumbl/lib/rumbl/repo.ex
@@ -1,30 +1,3 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repository
-  """
-
-  alias Rumbl.User
-
-  def all(User) do
-    [
-      %User{id: "1", name: "Fernando", username: "fernando", password: 'abc123'},
-      %User{id: "2", name: "Denise", username: "denise", password: 'abc123'},
-      %User{id: "3", name: "Sofia", username: "sofia", password: 'abc123'}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find(all(module), fn map -> map.id == id end)
-  end
-
-  def get_by(module, params) do
-    Enum.find(
-      all(module),
-      fn map ->
-	Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-      end
-    )
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
