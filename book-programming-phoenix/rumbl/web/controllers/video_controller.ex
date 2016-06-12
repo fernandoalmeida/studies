7cb5e1706803e4940fd54927b4ad5040c6a0a766
Fernando Almeida <fernando@fernandoalmeida.net>
2016-06-12 00:08:42 -0300

    [book-phoenix] Use `User` relationship on `VideoController`

--- book-programming-phoenix/rumbl/web/controllers/video_controller.ex
+++ book-programming-phoenix/rumbl/web/controllers/video_controller.ex
@@ -6,17 +6,22 @@ defmodule Rumbl.VideoController do
   plug :scrub_params, "video" when action in [:create, :update]
 
   def index(conn, _params, user) do
-    videos = Repo.all(Video)
+    videos = Repo.all(user_videos(user))
     render(conn, "index.html", videos: videos)
   end
 
   def new(conn, _params, user) do
-    changeset = Video.changeset(%Video{})
+    changeset = user
+    |> build_assoc(:videos)
+    |> Video.changeset()
+
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"video" => video_params}, user) do
-    changeset = Video.changeset(%Video{}, video_params)
+    changeset = user
+    |> build_assoc(:videos)
+    |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
       {:ok, _video} ->
@@ -29,18 +34,18 @@ defmodule Rumbl.VideoController do
   end
 
   def show(conn, %{"id" => id}, user) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(user), id)
     render(conn, "show.html", video: video)
   end
 
   def edit(conn, %{"id" => id}, user) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(user), id)
     changeset = Video.changeset(video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "video" => video_params}, user) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(user), id)
     changeset = Video.changeset(video, video_params)
 
     case Repo.update(changeset) do
@@ -54,7 +59,7 @@ defmodule Rumbl.VideoController do
   end
 
   def delete(conn, %{"id" => id}, user) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(user), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -71,5 +76,9 @@ defmodule Rumbl.VideoController do
       action_name(conn),
       [conn, conn.params, conn.assigns.current_user]
     )
+  end
+
+  defp user_videos(user) do
+    assoc(user, :videos)
   end
 end
