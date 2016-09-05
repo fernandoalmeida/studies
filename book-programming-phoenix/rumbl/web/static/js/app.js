bd551591b40aec998a17c110422a9bd1a4b690b4
Fernando Almeida <fernando@fernandoalmeida.net>
2016-09-05 13:46:27 -0300

    [book-phoenix] Add `VideoChannel` and socket config

--- book-programming-phoenix/rumbl/web/static/js/app.js
+++ book-programming-phoenix/rumbl/web/static/js/app.js
@@ -1,11 +1,4 @@
-import "phoenix_html"
+import socket from "./socket"
+import Video from "./video"
 
-import Player from "./player"
-
-let video = document.getElementById("video")
-
-if (video) {
-    Player.init(video.id, video.getAttribute("data-player-id"), () => {
-	console.log("player ready!")
-    })
-}
+Video.init(socket, document.getElementById("video"))
---
+++ book-programming-phoenix/rumbl/web/static/js/video.js
@@ -0,0 +1,30 @@
+import Player from "./player"
+
+let Video = {
+    init(socket, element) {
+	if(!element) { return }
+
+	let playerId = element.getAttribute("data-player-id")
+	let videoId = element.getAttribute("data-id")
+
+	socket.connect()
+
+	Player.init(element.id, playerId, () => {
+	    this.onReady(videoId, socket)
+	})
+    },
+
+    onReady(videoId, socket) {
+	let msgContainer = document.getElementById("msg-container")
+	let msgInput = document.getElementById("msg-input")
+	let postButton = document.getElementById("msg-submit")
+	let videoChannel = socket.channel("videos:" + videoId)
+
+	videoChannel
+	    .join()
+	    .receive("ok", resp => console.log("joined the video channel"))
+	    .receive("error", reason => console.log("join failed", reason))
+    }
+}
+
+export default Video
