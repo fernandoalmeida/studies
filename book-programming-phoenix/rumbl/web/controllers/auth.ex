58c303788358cd9ead2032ba413aa55d26a352a9
Fernando Almeida <fernando@fernandoalmeida.net>
2016-09-06 03:15:11 -0300

    [book-phoenix] Authenticate video channel by token

--- book-programming-phoenix/rumbl/web/controllers/auth.ex
+++ book-programming-phoenix/rumbl/web/controllers/auth.ex
@@ -14,9 +14,9 @@ defmodule Rumbl.Auth do
 
     cond do
       user = conn.assigns[:current_user] ->
-	conn
+	put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
-	assign(conn, :current_user, user)
+	put_current_user(conn, user)
       true ->
 	assign(conn, :current_user, nil)
     end
@@ -24,7 +24,7 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -57,5 +57,13 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
